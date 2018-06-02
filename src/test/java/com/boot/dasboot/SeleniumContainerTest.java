baeb41e681406d038ac2f011eb03dea17213390f
xmarlem <inglembomarco@gmail.com>
2018-06-03 01:02:49 +0200

    .

--- src/test/java/com/boot/dasboot/SeleniumContainerTest.java
+++ src/test/java/com/boot/dasboot/SeleniumContainerTest.java
@@ -101,7 +101,7 @@ public class SeleniumContainerTest {
 
         //This loop will rotate for 25 times to check If page Is ready after every 1 second.
         //You can replace your value with 25 If you wants to Increase or decrease wait time.
-        for (int i=0; i<100; i++){
+        for (int i=0; i<200; i++){
             try {
                 Thread.sleep(1000);
             }catch (InterruptedException e) {}
